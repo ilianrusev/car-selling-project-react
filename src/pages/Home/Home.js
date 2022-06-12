347a174230437b2894e85c8c005bb4f5dfcb8d07
Iliyan Rusev <ilianrusev@gmail.com>
2022-06-12 16:36:58 +0300

    home page added

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import BodyCategoriesSection from "../../components/BodyCategories/BodyCategoriesSection"
+import LatestOffersSection from "../../components/LatestOffersSection/LatestOffersSection"
+import SearchSection from "../../components/SearchSectionHome/SearchSection"
+import MainPageLayout from "../MainPageLayout/MainPageLayout"
+
+
+const Home = () => {
+    return (
+        <MainPageLayout>
+            <SearchSection />
+            <BodyCategoriesSection />
+            <LatestOffersSection />
+        </MainPageLayout>
+
+    )
+}
+
+export default Home
