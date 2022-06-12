d697a4894183f2963530220f442b8d6ecedf6486
Iliyan Rusev <ilianrusev@gmail.com>
2022-06-12 16:37:43 +0300

    catalog and about page added

---
+++ src/pages/About/About.js
@@ -0,0 +1,13 @@
+import Aside from "../../components/CatalogSection/Filters/Aside"
+import MainPageLayout from "../MainPageLayout/MainPageLayout"
+
+
+const About = () => {
+    return (
+        <MainPageLayout>
+            <Aside />
+        </MainPageLayout>
+    )
+}
+
+export default About
---
+++ src/pages/Catalog/Catalog.js
@@ -0,0 +1,13 @@
+import CatalogCard from "../../components/CatalogSection/CatalogCard/CatalogCard"
+import MainPageLayout from "../MainPageLayout/MainPageLayout"
+
+const Catalog = (props) => {
+    return (
+
+        <MainPageLayout>
+            <CatalogCard />
+        </MainPageLayout>
+    )
+}
+
+export default Catalog
