0f81be9421f142066f685ae5c11b7b4161493bea
Iliyan Rusev <ilianrusev@gmail.com>
2022-06-12 16:33:21 +0300

    router component

---
+++ src/components/Router/Router.js
@@ -0,0 +1,24 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Catalog from "../../pages/Catalog/Catalog";
+import LoadingSpinner from "../Spinner/Spinner";
+
+const NotFound = lazy(() => import("../../pages/404/NotFound"));
+const About = lazy(() => import("../../pages/About/About"));
+const Home = lazy(() => import('../../pages/Home/Home.js'));
+
+
+const AppRouter = () => {
+    return (
+        <Suspense fallback={<LoadingSpinner />}>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/catalog' element={<Catalog />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </Suspense>
+    )
+}
+
+export default AppRouter
