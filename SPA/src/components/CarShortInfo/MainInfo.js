8ef6ed7b189462b47937964da820123f5b6df0a9
Iliyan Rusev <ilianrusev@gmail.com>
2022-07-08 19:11:11 +0300

    short info box

--- SPA/src/components/CarShortInfo/MainInfo.js
+++ SPA/src/components/CarShortInfo/MainInfo.js
@@ -1,15 +1,27 @@
-import { Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 import InfoBox from '../Badge/Badge'
-import style from './MainInfo,module.css'
+
+import style from './MainInfo.module.css'
+
+import speedometer from '../../assets/images/speedometer.png'
+import location from '../../assets/images/location.png'
 
 const MainInfo = () => {
     return (
-        <section>
-            <div className={`ml-0 text-light d-flex align-items-center`}>
+        <section className=''>
+            <Row className={`ml-0 text-light d-flex align-items-center`}>
                 <h3 className='font-weight-bold mb-0 mr-3'>$31,900</h3>
                 <InfoBox property={'Used'} />
-                <InfoBox property={'Certified'}/>
-            </div>
+                <InfoBox property={'Certified'} />
+            </Row>
+            <Row className={`ml-0 mt-4 text-light ${style.footer}`}>
+                <Col className={`ml-0 align-items-center p-0 ${style.date}`}>
+                    <span><img src={speedometer} alt='speedometer' />25K KM</span>
+                </Col>
+                <Col className={`align-items-center`}>
+                    <span><img src={location} alt='location' />Chicago</span>
+                </Col>
+            </Row>
         </section>
     )
 }
