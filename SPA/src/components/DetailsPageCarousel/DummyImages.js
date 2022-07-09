9e0edb53d8e29d934b255ab91d69776f22ab21ab
Iliyan Rusev <ilianrusev@gmail.com>
2022-07-09 04:57:11 +0300

    replaced pics for test

--- SPA/src/components/DetailsPageCarousel/DummyImages.js
+++ SPA/src/components/DetailsPageCarousel/DummyImages.js
@@ -1,29 +1,29 @@
 import merc from '../../assets/images/merc.jpg'
 
 const DUMMY_IMAGES = [
-    <div style={{ width: '100%', height: '100%' }} key='12312'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_MG.jpg' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='12312'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Fd.jpg' alt='car' />
     </div>,
-    <div style={{ width: '100%', height: '100%' }} key='123ffsdfsdfs12'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_LX.jpg' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='123ffsdfsdfs12'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Y.jpg' alt='car' />
     </div>,
-    <div style={{ width: '100%', height: '100%' }} key='123rwerwe12'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_tE.jpg' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='123rwerwe12'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_5j.jpg' alt='car' />
     </div>,
-    <div style={{ width: '100%', height: '100%' }} key='123fsdfsd12'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_gI.png' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='123fsdfsd12'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_1z.jpg' alt='car' />
     </div>,
-    <div style={{ width: '100%', height: '100%' }} key='12adasdasdafs312'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_lP.jpg' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='12adasdasdafs312'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Ty.jpg' alt='car' />
     </div>,
-    <div style={{ width: '100%', height: '100%' }} key='12ggfdgd312'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_bt.jpg' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='12ggfdgd312'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_nb.jpg' alt='car' />
     </div>,
-    <div style={{ width: '100%', height: '100%' }} key='12gdfg312'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_6D.jpg' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='12gdfg312'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_gW.jpg' alt='car' />
     </div>,
-    <div style={{ width: '100%', height: '100%' }} key='123rwerwf312'>
-        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_SN.jpg' alt='car' />
+    <div tyle={{ width: '100%', height: '100%' }} key='123rwerwf312'>
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Sf.jpg' alt='car' />
     </div>
 ]
 
--- SPA/src/components/DetailsPageCarousel/DummyImages2.js
+++ SPA/src/components/DetailsPageCarousel/DummyImages2.js
@@ -1,29 +1,28 @@
-import merc from '../../assets/images/merc.jpg'
 
 const DUMMY_IMAGES = [
     <div key='12312'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_MG.jpg' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Fd.jpg' alt='car' />
     </div>,
     <div key='123ffsdfsdfs12'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_LX.jpg' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Y.jpg' alt='car' />
     </div>,
     <div key='123rwerwe12'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_tE.jpg' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_5j.jpg' alt='car' />
     </div>,
     <div key='123fsdfsd12'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_gI.png' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_1z.jpg' alt='car' />
     </div>,
     <div key='12adasdasdafs312'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_lP.jpg' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Ty.jpg' alt='car' />
     </div>,
     <div key='12ggfdgd312'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_bt.jpg' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_nb.jpg' alt='car' />
     </div>,
     <div key='12gdfg312'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_6D.jpg' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_gW.jpg' alt='car' />
     </div>,
     <div key='123rwerwf312'>
-        <img style={{ width: 'auto', height: '70px' }} src='https://mobistatic4.focus.bg/mobile/photosmob/841/2/big/21620768350740841_SN.jpg' alt='car' />
+        <img style={{ width: '100%', height: '100%' }} src='https://mobistatic1.focus.bg/mobile/photosmob/294/2/big/21649418615733294_Sf.jpg' alt='car' />
     </div>
 ]
 
